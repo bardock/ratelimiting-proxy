edad5b813ae395e7bc83a0256b7227f3ab4ca675
bardock <fedesanchez89@gmail.com>
2017-08-01 08:48:24 -0300

    move ts output to build folder

--- control-admin/services/rules/aws-ka/CreateCommand.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=CreateCommand.js.map
--- control-admin/services/rules/aws-ka/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AWS = require("aws-sdk");
-AWS.config.update({ region: 'us-east-1' });
-exports.default = new AWS.KinesisAnalytics();
-//# sourceMappingURL=client.js.map
